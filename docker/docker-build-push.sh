1332a74a5198949d5dff2f288e901ddf3ecf94b0
Rajesh Iyer <iyerajesh@gmail.com>
2020-03-08 14:55:29 -0400

    Updated docker-build-push.sh

--- docker/docker-build-push.sh
+++ docker/docker-build-push.sh
@@ -5,4 +5,4 @@ export PROJECT=$(gcloud config --quiet get-value project)
 ./gradlew clean build jibDockerBuild
 docker push gcr.io/$PROJECT/springboot-jib-demo:latest
 
-# docker run --name springboot-jib-demo -d gcr.io/xylia-platform/springboot-jib-demo:latest
+# docker run --name springboot-jib-demo -p 8080:8080 -d gcr.io/xylia-platform/springboot-jib-demo:latest
